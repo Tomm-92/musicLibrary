178e3f66ef503d00508602c71d08e7880d74ae2f
Tom Wilkinson <thomasmarkwilkinson@gmail.com>
2023-02-18 16:31:29 +0000

    second challenge

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,16 +1,28 @@
 const db = require('../db/index');
 
 
-const createArtist = async (req, res) => {
+exports.createArtist = async (req, res) => {
     const { name, genre } = req.body
   
     try {
-      const { rows: [ artist ] } = await db.query(`INSERT INTO Artists (name, genre) VALUES ('${name}', '${genre}') RETURNING *`)
-      res.status(201).json(artist)
-    } catch (err) {
-      res.status(500).json(err.message)
+        const { rows: [ artist ] } = await db.query('INSERT INTO Artists (name, genre) VALUES ($1, $2) RETURNING *', [name, genre])
+        res.status(201).json(artist)
+    } 
+      catch (err) {
+        res.status(500).json(err.message)
+      }
     }
-  }
-  
-  module.exports = { createArtist }
+
+    exports.read = async (req, res) => {
+        try {
+            const { rows } = await db.query('SELECT * FROM Artists')
+        res.status(200).json(rows)
+        }
+        catch (err) {
+            res.status(500).json(err.message)
+          }
+        }
+    
+
+
 
